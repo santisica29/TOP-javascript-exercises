bd4d2c1c97483174fd3fd23ae6e2866dd7f5e9b7
Santiago Sica <santisica29@gmail.com>
2025-01-04 12:59:34 -0300

    finish ex3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = (string) => {
+    let array = string.split("").reverse();
 
-};
+    let reversedString = array.join("");
+
+    return reversedString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
