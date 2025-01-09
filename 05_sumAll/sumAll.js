add06a3f40d4801b5b3db2526227039dcab93719
Santiago Sica <santisica29@gmail.com>
2025-01-09 12:34:06 -0300

    finish ex5 and all the validations

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (initialNum, finalNumber) {
+  let sum = 0;
 
+  if ((initialNum < 0 || finalNumber < 0) || (!Number.isInteger(initialNum) || !Number.isInteger(finalNumber)) || (isNaN(initialNum) || isNaN(finalNumber))) {
+    return "ERROR";
+  }
+
+  if (initialNum > finalNumber) {
+    let helper = initialNum;
+    initialNum = finalNumber;
+    finalNumber = helper;
+  }
+
+  for (let i = initialNum; i <= finalNumber; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
