9f7b6e36d123bb66f42cba803f15d6111efbd517
Santiago Sica <santisica29@gmail.com>
2025-01-10 11:23:51 -0300

    finish ex6 and ex7 of arrays js section ins top

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
+const leapYears = year => {
+    let isDivisibleByFour = year % 4 === 0;
+    let isDivisibleBy100 = year % 100 === 0;
+    let isDivisibleBy400 = year % 400 === 0;
 
+    return isDivisibleBy400 || (isDivisibleByFour && !isDivisibleBy100);
 };
 
+leapYears(1900);
+
+/*Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years)*/
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(numInF) {
+  let numInC = (numInF - 32) * (5/9);
+  let numRoundUp = numInC.toFixed(1)
+  return Number(numRoundUp);
 };
+// (x − 32) × ⁠5/9⁠ °C 
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(numInC) {
+  let numInF = (numInC * (9/5) + 32);
+  let numRoundUp = numInF.toFixed(1)
+  return Number(numRoundUp);
 };
+// (x × ⁠9/5⁠ + 32) °F 
 
 // Do not edit below this line
 module.exports = {
