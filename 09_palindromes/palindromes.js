ba1140584c6fa30cc9a302493edfd7125207e1d0
Santiago Sica <santisica29@gmail.com>
2025-09-05 15:48:52 -0300

    finish ex9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  let abc = "abcdefghijklmnopqrstvwxyz0123456789";
 
+  word = word
+    .toLowerCase()
+    .split("")
+    .filter((x) => abc.includes(x))
+    .join("");
+
+
+  let wordBackwards = word
+    .split("")
+    .reverse()
+    .join("");
+
+  return word === wordBackwards;
 };
 
 // Do not edit below this line
