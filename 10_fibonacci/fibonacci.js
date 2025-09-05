d7829456ff6718b611d6db76e586de75d4e99a09
Santiago Sica <santisica29@gmail.com>
2025-09-05 16:21:15 -0300

    finish ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(numLimit) {
+    if (numLimit < 0) return 'OOPS'
+    let n1 = 0;
+    let n2 = 1;
+    let fiboNum = 0;
 
+    for (let i=0; i < numLimit; i++){
+        fiboNum = n1 + n2;
+
+        n1 = n2;
+        n2 = fiboNum;
+
+    }
+
+    return n1;
 };
+
+fibonacci(3)
 
 // Do not edit below this line
 module.exports = fibonacci;
