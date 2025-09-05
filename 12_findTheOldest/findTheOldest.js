35c637d9cc398cfcb65ea10922fbc830dc40bcc3
Santiago Sica <santisica29@gmail.com>
2025-09-05 19:34:24 -0300

    finish ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
 
+
+const findTheOldest = function (arr) {
+  let sortArr = arr.sort((a, b) => {
+    let currentYear = new Date().getFullYear();
+    if (a.yearOfDeath === undefined) {
+      a.yearOfDeath = currentYear;
+    } 
+    
+    if (b.yearOfDeath === undefined) {
+      b.yearOfDeath = currentYear;
+    }
+
+    let age1 = (a.yearOfDeath - a.yearOfBirth);
+    let age2 = (b.yearOfDeath - b.yearOfBirth);
+    return age2 - age1
+  });
+
+  let oldestPerson = sortArr[0]
+
+  return oldestPerson;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
